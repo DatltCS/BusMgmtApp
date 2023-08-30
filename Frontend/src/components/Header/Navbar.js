c3a4ab40b391e4dcddd9dcd3ca8e8f7f4e894eb9
dev_quynh <nhuquynh06032002@gmail.com>
2023-08-30 12:52:10 +0700

    update register form frontend

--- Frontend/src/components/Header/Navbar.js
+++ Frontend/src/components/Header/Navbar.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import { Link, useNavigate } from "react-router-dom";
 import { MyUserContext } from "../../App";
+import { Button, Col, Container, Form, Nav, NavDropdown, Row } from "react-bootstrap";
 
 
 import './Navbar.css';
@@ -31,11 +32,17 @@ function Navbar() {
             <li><Link to="/sign-in"><button className="login-button">Đăng nhập</button></Link></li>
             <li><Link to="/register"><button className="login-button">Đăng ký</button></Link></li>
           </> : <>
-            <Link to="/home">
-              <a>Chào {user.username}</a>
-            </Link>
-            <li><Link to="/register"><button className="login-button" onClick={logout}>Đăng xuất</button></Link></li>
+            <Link className="nav-link text-danger" to="/">Chào {user.username}!</Link>
+            
+            <li><button className="login-button" onClick={logout}>Đăng xuất</button></li>
           </>}
+          {/* {user === null ? <>
+                                <Link className="nav-link text-danger" to="/login">Đăng nhập</Link>
+                                <Link className="nav-link text-danger" to="/register">Đăng ký</Link>
+                            </> : <>
+                                <Link className="nav-link text-danger" to="/">Chào {user.username}!</Link>
+                                <Button variant="secondary" onClick={logout}>Đăng xuất</Button>
+                            </>} */}
         </ul>
       </nav>
 
